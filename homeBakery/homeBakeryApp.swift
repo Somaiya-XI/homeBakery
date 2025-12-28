e5841f1ee9b838a9d62c99f497afbd9c1642b3ce
Somaiya <4111674@upm.edu.sa>
2025-12-28 13:09:35 +0300

    Initial Commit

---
+++ homeBakery/homeBakeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeBakeryApp.swift
+//  homeBakery
+//
+//  Created by Somaiya on 08/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct homeBakeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
